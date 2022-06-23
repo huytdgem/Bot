b57e9c7ddf15013735599dacc2beecff7b5a9cfa
Trinh Duc Huy <trinhduchuy@HuyTd-Imac.local>
2022-06-23 14:30:21 +0700

    init bot daily meeting

--- routes/ReminderInGroupWithBoss/index.js
+++ routes/ReminderInGroupWithBoss/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 const schedule = require('node-schedule');
 const axios = require('axios')
-const {ALL_USER} = require("../const");
+const {ALL_USER} = require("../../const");
 
 const url = 'https://chat.googleapis.com/v1/spaces/AAAALKMqWMc/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=xP7MzeyVd1iVooz1T2xiRrM-L2sewkMHwznE8JB2KfY%3D'
 
@@ -12,20 +12,21 @@ router.get('/', function(req, res, next) {
 });
 
 const rule = new schedule.RecurrenceRule();
-rule.dayOfWeek = [new schedule.Range(1, 6)];
+rule.dayOfWeek = [new schedule.Range(1, 5)];
 rule.hour = 17;
 rule.minute = 30;
 
 schedule.scheduleJob(rule, function(){
   axios.post(url, {
       "text": `${ALL_USER} Report đi anh emm ơiii!!`
-  }).then(res => {
-    console.log(`statusCode: ${res.status}`)
-    console.log(res)
-  })
-      .catch(error => {
+  }).catch(error => {
         console.error(error)
       })
-});
+})
+axios.post(url, {
+    "text": `Bot nhắc nhở báo cáo được khởi chạy.`
+}).catch(error => {
+        console.error(error)
+    })
 
 module.exports = router;
---
+++ routes/ReminderInGroupWithTeam/index.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var router = express.Router();
+const schedule = require('node-schedule');
+const axios = require('axios')
+const {ALL_USER} = require("../../const");
+
+const url = 'https://chat.googleapis.com/v1/spaces/AAAAYjXH928/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=gLz6UTxHLrqdWLqbhaXizpWlj67uxtc3G3UvvEOqNwA%3D'
+const message = `${ALL_USER} Chuẩn bị họp Daily!! \n
+            Họp không quá 15p. Và không thảo luận quá sâu để giải quyết vấn đề.\n Mọi người có thể họp riêng với nhau sau daily meeting. \n
+            Lưu ý theo format: \n 
+            1. Đã làm những gì ngày hôm trước? \n 
+            2. Sẽ làm những gì ngày hôm nay? \n 
+            3. Có vấn đề gì ảnh hưởng đến tiến độ. \n
+      `
+
+const rule = new schedule.RecurrenceRule();
+rule.dayOfWeek = [new schedule.Range(1, 5)];
+rule.hour = 8;
+rule.minute = 50;
+
+schedule.scheduleJob(rule, function(){
+
+  axios.post(url, {
+      "text": message
+  }).catch(error => {
+        console.error(error)
+      })
+});
+
+axios.post(url, {
+    "text": `Bot reminder nhắc nhở daily được khởi chạy.`
+}).catch(error => {
+    console.error(error)
+})
+
+module.exports = router;
